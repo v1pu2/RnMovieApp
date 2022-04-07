7eb3ff4cb722b51268d3495ced41a74b38ad5978
VJ <v1bhalodia@gmail.com>
2022-04-07 11:28:14 +0530

    set ui in movie detail screen

--- src/screens/MovieDetail.js
+++ src/screens/MovieDetail.js
@@ -7,30 +7,40 @@ import {
   Text,
   useColorScheme,
   View,
+  ImageBackground,
 } from 'react-native';
+import moment from 'moment';
 
 const MovieDetail = props => {
   const isDarkMode = useColorScheme() === 'dark';
-  console.log('in detal', props?.route?.params);
+  const movie = props?.route?.params?.item;
   const backgroundStyle = {
     backgroundColor: isDarkMode ? '#000000' : '#ffffff',
   };
+  const baseImage = 'https://image.tmdb.org/t/p/original';
+
+  const imgUrl = baseImage + movie?.backdrop_path;
+
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? '#000000' : '#ffffff',
-          }}>
-          <Text style={{color: isDarkMode ? '#ffffff' : '#000000'}}>
-            this is app MovieDetail
+    <ImageBackground
+      source={{uri: `${imgUrl}`}}
+      resizeMode="cover"
+      style={styles.image}>
+      <View style={styles.textView}>
+        <Text style={styles.textTitle}>{movie?.title}</Text>
+        <Text style={styles.txtPercentage}>
+          {moment(movie?.release_date).format('MMMM DD, YYYY')}
+        </Text>
+        <View style={styles.rowView}>
+          <Text style={[styles.txtPercentage, {paddingRight: 60}]}>
+            {movie?.vote_average}%
           </Text>
+          <Text style={styles.txtPercentage}>2 hr 30 min</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+        <Text style={styles.txtPercentage}>{movie?.overview}</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -38,6 +48,31 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  textView: {
+    width: '90%',
+    backgroundColor: '#000000c0',
+    alignSelf: 'center',
+    paddingBottom: 80,
+    padding: 10,
+  },
+  textTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  rowView: {
+    flexDirection: 'row',
+  },
+  txtPercentage: {
+    color: 'white',
+    fontSize: 14,
+    marginBottom: 10,
   },
 });
 
